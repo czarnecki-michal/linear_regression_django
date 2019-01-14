e5ba7fee088920e2251696fbde9c192faeffe2cb
czarnecki-michal <nwasm30@gmail.com>
2019-01-14 15:19:01 +0100

    .

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
 from . import views
+from .views import ChartsView, get_data
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('regression', views.detail, name='detail')
+    path('data', views.detail, name='detail'),
+    path('info', views.info, name='info'),
+    path('charts', ChartsView.as_view(), name='charts'),
+    path('api/data/', get_data, name="api-data"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template import loader
+from django.views.generic import View
 
 from .models import Data
 
@@ -11,3 +12,20 @@ def detail(request):
     train_data = Data.objects.all()[:47]
     test_data = Data.objects.all()[47:]
     return render(request, 'projects/data.html', {'train_data': train_data, 'test_data': test_data})
+
+def info(request):
+    return render(request, 'projects/info.html')
+
+def charts(request):
+    return render(request, 'projects/charts.html')
+
+class ChartsView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'projects/charts.html')
+
+def get_data(request):
+    data = {
+        "sales": 100,
+        "customers": 10,
+    }
+    return JsonResponse(data)
