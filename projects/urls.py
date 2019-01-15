cf8c040019727ae98b62e6784d4ee3b2eb1402b1
czarnecki-michal <nwasm30@gmail.com>
2019-01-15 22:09:30 +0100

    .

--- projects/urls.py
+++ projects/urls.py
@@ -5,9 +5,7 @@ from .views import get_data, calculate, reset
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('data', views.detail, name='detail'),
-    path('info', views.info, name='info'),
-    path('charts', views.charts, name='charts'),
+    path('detail', views.detail, name='detail'),
     path('api/data/', get_data, name="api-data"),
     path('api/calculate', calculate, name="calculate"),
     path('api/reset', reset, name="reset"),
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, Http404
 from django.template import loader
 from django.views.generic import View
 from django.shortcuts import redirect
@@ -11,27 +11,31 @@ def index(request):
     return render(request, 'projects/index.html')
 
 def detail(request):
-    train_data = Data.objects.all()[:47]
-    test_data = Data.objects.all()[47:]
-    return render(request, 'projects/data.html', {'train_data': train_data, 'test_data': test_data})
+    try:
+        train_data = Data.objects.all()[:47]
+        test_data = Data.objects.all()[47:]
+        return render(request, 'projects/detail.html', {'train_data': train_data, 'test_data': test_data})
+    except:
+        return render(request, 'projects/detail.html')
 
-def info(request):
-    return render(request, 'projects/info.html')
 
 def charts(request):
     return render(request, 'projects/charts.html')
 
 def get_data(request):
-    worked_years = [data.workedYears for data in Data.objects.all()]
-    salary_brutto = [data.salaryBrutto for data in Data.objects.all()]
-    data = []
+    try:
+        worked_years = [data.workedYears for data in Data.objects.all()]
+        salary_brutto = [data.salaryBrutto for data in Data.objects.all()]
+        data = []
 
-    for val in range(len(worked_years)):
-        data.append({
-            'x': worked_years[val],
-            'y': salary_brutto[val],
-        })
-    return JsonResponse(data, safe=False)
+        for val in range(len(worked_years)):
+            data.append({
+                'x': worked_years[val],
+                'y': salary_brutto[val],
+            })
+        return JsonResponse(data, safe=False)
+    except:
+        return render(request, 'projects/detail.html')
 
 def calculate(request):
     reg = LinearRegression()
@@ -45,13 +49,12 @@ def calculate(request):
 
     reg.fit(train_X, train_y)
 
-    test_x = [row.workedYears for row in data[47:]]
     for i in Data.objects.all():
-        if(i.salaryBrutto == None):
+        if(i.salaryBrutto is None):
             i.salaryBrutto = reg.predict([i.workedYears])[0]
             i.save()
 
-    return redirect('/data')
+    return redirect('/detail#data')
 
 def reset(request):
     test_x = [row.workedYears for row in Data.objects.all()[47:]]
@@ -62,4 +65,4 @@ def reset(request):
         q.save()
         id += 1
 
-    return redirect('/data')
+    return redirect('/detail#data')
