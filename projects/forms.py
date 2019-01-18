a8ba229026c01ccb9aa1407243641acf57e4258d
czarnecki-michal <nwasm30@gmail.com>
2019-01-18 11:18:09 +0100

    .

--- projects/forms.py
+++ projects/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class ChartForm(forms.Form):
-    workedYears_min = forms.IntegerField(label="worked years min", max_value=50000, min_value=0, initial=0)
-    workedYears_max = forms.IntegerField(label="worked years max", max_value=50000, min_value=0, initial=100)
+    workedYears_min = forms.IntegerField(label="worked years min", max_value=100, min_value=0, initial=0)
+    workedYears_max = forms.IntegerField(label="worked years max", max_value=100, min_value=0, initial=100)
 
     def clean(self):
         cleaned_data = super().clean()
